e204f87c47e3bfbe821666e48caa6b7552be49e0
Nazmul Hoque <coder.nazmul@gmail.com>
2020-03-03 12:59:30 +0600

    template system is practiced

---
+++ template.js
@@ -0,0 +1,10 @@
+let firstName = "Nazmul";
+let lastName = "Hoque";
+
+const fullName = `${firstName} ${lastName} is a pasionate guitarist.`; //it's a easier system to give a whiteSpace.
+console.log(fullName);
+
+// New line also easily possible in es6
+console.log(`I will go to my office.
+After come back my office.
+i will call my wife "I love You, janotos"`);
